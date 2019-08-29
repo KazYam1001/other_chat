9e2201d817aee40b347a74f5561b32d8e059ed14
Kazushi Yamanishi <keironia187149@gmail.com>
2019-08-30 01:50:56 +0900

    edit carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,21 +4,21 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
 
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    # aws_access_key_id: ENV['OC_AWS_ACCESS_KEY'],
-    # aws_secret_access_key: ENV['OC_AWS_SECRET'],
-    region: 'ap-northeast-1'
-  }
-  config.fog_directory  = 'yamprochat'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamprochat'
-  # if Rails.env.production?
-  # else
-  #   config.storage :file
-  # end
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      aws_access_key_id: ENV['OC_AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['OC_AWS_SECRET'],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'yamprochat'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamprochat'
+  else
+    config.storage :file
+  end
 
 end
