4ed6ce4c80cb6514bf634b2f0fd5eb6bb415aaec
Kazushi Yamanishi <keironia187149@gmail.com>
2019-08-30 11:30:42 +0900

    add bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,10 +9,10 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      aws_access_key_id: ENV['OC_AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['OC_AWS_SECRET'],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      # aws_access_key_id: ENV['OC_AWS_ACCESS_KEY'],
+      # aws_secret_access_key: ENV['OC_AWS_SECRET'],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'yamprochat'
